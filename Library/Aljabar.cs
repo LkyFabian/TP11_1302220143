6f1ec14ca8dae741469cb4d6bc4afcedd36b7920
Jeremy Fabian <118284208+LkyFabian@users.noreply.github.com>
2024-05-06 02:55:12 +0700

    TP 11

---
+++ Library/Aljabar.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Aljabar
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double d = (b * b) - (4 * a * c);
+
+            if (d < 0)
+            {
+                return new double[0]; // tidak ada akar real
+            }
+            else if (d == 0)
+            {
+                double x = -b / (2 * a);
+                return new double[] { x };
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                return new double[] { x1, x2 };
+            }
+        }
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double a2 = a * a;
+            double b2 = 2 * a * b;
+            double c2 = b * b;
+
+            return new double[] { a2, b2, c2 };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Library;
+using System;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] persamaan = { 1, -3, -10 };
+            double[] akar = Aljabar.AkarPersamaanKuadrat(persamaan);
+            double[] hasilKuadrat = Aljabar.HasilKuadrat(new double[] { 2, -3 });
+
+            Console.WriteLine(" Berikut akar persamaan kuadrat:");
+            foreach (var akarValue in akar)
+            {
+                Console.WriteLine(akarValue);
+            }
+
+            Console.WriteLine("\n Berikut hasil kuadrat persamaan berpangkat 1:");
+            foreach (var value in hasilKuadrat)
+            {
+                Console.WriteLine(value);
+            }
+
+        }
+    }
+}
